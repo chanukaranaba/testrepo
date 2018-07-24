1b30519a3476b345fa6c01f91cf9e6e5639b9416
Chanuka Dissanayake <chanuka@Chanukas-MacBook-Pro.local>
2018-07-24 12:37:12 -0300

    adding project

---
+++ JsonPayloadClassMeditor/src/main/java/com/example/jsonpayload/ProcessData.java
@@ -0,0 +1,12 @@
+package com.example.jsonpayload;
+
+import org.apache.synapse.MessageContext; 
+import org.apache.synapse.mediators.AbstractMediator;
+
+public class ProcessData extends AbstractMediator { 
+
+	public boolean mediate(MessageContext context) { 
+		// TODO Implement your mediation logic here 
+		return true;
+	}
+}
